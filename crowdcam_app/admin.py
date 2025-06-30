36d27b99fe0609cf2c15c6878fa3ec613e527400
ngapox <chukizzle@gmail.com>
2025-06-30 12:20:29 +0300

    Create Event model and register in admin

--- crowdcam_app/admin.py
+++ crowdcam_app/admin.py
@@ -1,3 +1,7 @@
+# crowdcam_app/admin.py
+
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+admin.site.register(Event)
--- crowdcam_app/models.py
+++ crowdcam_app/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# crowdcam_app/models.py
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    event_date = models.DateField()
+    unique_code = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.unique_code:
+            self.unique_code = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
